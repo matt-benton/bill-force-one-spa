5197d5175348d22a4a41f2af650d4e8237590c48
Matt Benton <mattbenton@Luca.local>
2020-05-08 12:35:28 -0500

    Add welcome page

---
+++ frontend/src/pages/Welcome.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="welcome-container">
+        <div class="welcome-panel">
+            <h1 class="main-heading">Bill Force One</h1>
+            <h3 class="sub-heading">Track Your Monthly Expenses: Know What's Due And When</h3>
+            <ul class="welcome-menu">
+                <li class="welcome-menu-item">
+                    <router-link to="/accounts">
+                        <button class="btn btn-secondary">Accounts</button>
+                    </router-link>
+                </li>
+                <li class="welcome-menu-item">
+                    <router-link to="/accounts/create">
+                        <button class="btn btn-secondary">New Account</button>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,9 @@
+import Welcome from './pages/Welcome'
 import Accounts from './pages/Accounts'
 import CreateAccount from './pages/CreateAccount'
 
 export const routes = [
+    { path: '', component: Welcome },
     { path: '/accounts', component: Accounts },
     { path: '/accounts/create', component: CreateAccount },
 ]
