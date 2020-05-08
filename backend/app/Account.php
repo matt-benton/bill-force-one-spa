2237320b35587c95a151cb087681576e090bb921
Matt Benton <mattbenton@Luca.local>
2020-05-08 17:16:12 -0500

    Add ability to create accounts

--- backend/app/Account.php
+++ backend/app/Account.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
+    protected $fillable = ['name', 'description'];
+
     public function bills()
     {
         return $this->hasMany('App\Bill');
--- frontend/src/pages/CreateAccount.vue
+++ frontend/src/pages/CreateAccount.vue
@@ -1,21 +1,53 @@
 <template>
     <div class="form-container">
-        <form class="form" action="/accounts" method="POST">
+        <form class="form">
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" name="name" />
+                <input type="text" name="name" v-model="name" />
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <textarea name="description" rows="3"></textarea>
+                <textarea name="description" rows="3" v-model="description"></textarea>
             </div>
-            <button class="btn btn-primary" type="submit">Create Account</button>
+            <button class="btn btn-primary" @click.prevent="submit">Create Account</button>
         </form>
     </div>
 </template>
 
 <script>
-export default {}
+import gql from 'graphql-tag'
+
+export default {
+    data() {
+        return {
+            name: '',
+            description: '',
+        }
+    },
+    methods: {
+        submit() {
+            this.$apollo
+                .mutate({
+                    mutation: gql`
+                        mutation($name: String!, $description: String) {
+                            createAccount(name: $name, description: $description) {
+                                id
+                                name
+                                description
+                            }
+                        }
+                    `,
+                    variables: {
+                        name: this.name,
+                        description: this.description,
+                    },
+                })
+                .then((response) => {
+                    this.$router.push(`/accounts/${response.data.createAccount.id}/bills`)
+                })
+        },
+    },
+}
 </script>
 
 <style></style>
