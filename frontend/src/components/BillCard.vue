5cdfe1735ff1f4a546e211fb1567dcfe7dfd587d
Matt Benton <mattbenton@Luca.local>
2020-05-11 12:58:33 -0500

    Add bill card component

---
+++ frontend/src/components/BillCard.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="bills-card">
+        <div class="card-header">
+            <a href="#">
+                <h3>{{ bill.name }}</h3>
+            </a>
+            <span>{{ bill.amount }}</span>
+        </div>
+        <div class="card-body">
+            <p class="due-text">
+                Due on {{ getMonthName(bill) }} the {{ formatDueDateWithSuffix(bill) }}
+                <svg
+                    v-if="bill.warning"
+                    class="warning-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    version="1.1"
+                    viewBox="0 0 24 24"
+                >
+                    <path d="M12,2L1,21H23M12,6L19.53,19H4.47M11,10V14H13V10M11,16V18H13V16" />
+                </svg>
+            </p>
+            <p>{{ bill.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { format } from 'date-fns'
+
+export default {
+    methods: {
+        formatDueDateWithSuffix(bill) {
+            return format(new Date(2019, 1, bill.due_date), 'do')
+        },
+        getMonthName(bill) {
+            switch (bill.due_month) {
+                case 0:
+                    return
+                case 1:
+                    return 'January'
+                case 2:
+                    return 'February'
+                case 3:
+                    return 'March'
+                case 4:
+                    return 'April'
+                case 5:
+                    return 'May'
+                case 6:
+                    return 'June'
+                case 7:
+                    return 'July'
+                case 8:
+                    return 'August'
+                case 9:
+                    return 'September'
+                case 10:
+                    return 'October'
+                case 11:
+                    return 'November'
+                case 12:
+                    return 'December'
+            }
+        },
+    },
+    props: ['bill'],
+}
+</script>
+
+<style></style>
--- frontend/src/pages/AccountDashboard.vue
+++ frontend/src/pages/AccountDashboard.vue
@@ -1,23 +1,23 @@
 <template>
     <div>
         <h2>Account Dashboard</h2>
-        <ul>
-            <li v-for="bill in account.bills" :key="bill.id">
-                <h3>{{ bill.name }}</h3>
-                <p>{{ bill.description }}</p>
-            </li>
-        </ul>
+        <div v-if="$apollo.loading">Loading</div>
+        <bill-card v-for="bill in account.bills" :key="bill.id" :bill="bill" v-else></bill-card>
     </div>
 </template>
 
 <script>
 import gql from 'graphql-tag'
+import BillCard from '../components/BillCard'
 
 export default {
     data() {
         return {
             accountId: this.$route.params.accountId,
         }
+    },
+    components: {
+        'bill-card': BillCard,
     },
     apollo: {
         account: {
@@ -30,6 +30,8 @@ export default {
                             id
                             name
                             description
+                            due_month
+                            due_date
                         }
                     }
                 }
