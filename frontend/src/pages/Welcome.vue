6f253995bd1b76dfed1a620d8397c5ff7b313950
Matt Benton <mattbenton@Luca.local>
2020-05-13 12:10:22 -0500

    Add landing page styles

--- frontend/src/pages/Welcome.vue
+++ frontend/src/pages/Welcome.vue
@@ -23,4 +23,40 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.welcome-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(var(--color-grey-dark-2) 40%, var(--color-grey-dark-3) 40%);
+}
+
+.welcome-panel {
+    background-color: var(--color-primary-dark-3);
+    padding: var(--spacing-largest);
+    color: var(--color-white);
+    transform: translateY(-40%);
+}
+
+.main-heading {
+    font-size: var(--font-xl);
+    font-weight: 400;
+    margin-bottom: var(--spacing-medium);
+}
+
+.sub-heading {
+    font-size: var(--font-large);
+    font-weight: 400;
+    margin-bottom: var(--spacing-largest);
+}
+
+.welcome-menu {
+    display: flex;
+}
+
+.welcome-menu-item {
+    margin-right: var(--spacing-medium);
+}
+</style>
