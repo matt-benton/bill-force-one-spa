e8bb5a623b0bb4c69ec75607797f9c6e0a050035
Matt Benton <mattbenton@Luca.local>
2020-05-14 10:39:01 -0500

    Add bill sorting

--- frontend/src/pages/AccountDashboard.vue
+++ frontend/src/pages/AccountDashboard.vue
@@ -4,10 +4,37 @@
             <main-info-panel :account="account"></main-info-panel>
         </div>
         <div class="row">
+            <div class="bills-list-menu">
+                <form class="bills-order-form">
+                    <select class="bills-filter-select" id="bills-filter-select" name="filter">
+                        <option value="month">Due This Month</option>
+                        <option value="all">All Bills</option>
+                        <option value="yearly">All Yearly Bills</option>
+                    </select>
+                    <label for="bills-order-select">Order By</label>
+                    <select
+                        class="bills-order-select"
+                        id="bills-order-select"
+                        name="order"
+                        v-model="sortBy"
+                    >
+                        <option value="name">Name</option>
+                        <option value="amount">Amount</option>
+                        <option value="due_date">Due Date</option>
+                        <option value="paid">Paid Status</option>
+                    </select>
+                </form>
+                <div class="bills-list-menu-right">
+                    <router-link :to="`/accounts/${accountId}/bills/create`">
+                        <button class="btn btn-secondary">New Bill</button>
+                    </router-link>
+                    <button class="btn btn-secondary" type="submit">Reset Monthly Bills</button>
+                </div>
+            </div>
             <div class="bills-list">
                 <div v-if="$apollo.loading">Loading</div>
                 <bill-card
-                    v-for="bill in account.bills"
+                    v-for="bill in sortedBills"
                     :key="bill.id"
                     :bill="bill"
                     @paid-status-toggled="togglePaidStatus(bill)"
@@ -28,7 +55,24 @@ export default {
     data() {
         return {
             accountId: this.$route.params.accountId,
+            sortBy: 'name',
         }
+    },
+    computed: {
+        sortedBills: function() {
+            const bills = this.account.bills
+
+            return bills.sort((a, b) => {
+                switch (typeof a[this.sortBy]) {
+                    case 'string':
+                        return a[this.sortBy].localeCompare(b[this.sortBy])
+                    case 'number':
+                        return a[this.sortBy] - b[this.sortBy]
+                    case 'boolean':
+                        return b[this.sortBy] - a[this.sortBy]
+                }
+            })
+        },
     },
     methods: {
         togglePaidStatus(bill) {
